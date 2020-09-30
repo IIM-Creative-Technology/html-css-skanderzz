2fbff875c6b8d5438af83e1bb8d59817a795e833
Alexandre Delafosse <alexandre.delafosse@edu.devinci.fr>
2020-09-30 21:34:50 +0200

    filrouge

--- index.php
+++ index.php
@@ -3,18 +3,21 @@
 ?>
 <main>
     <h1> Que ce passera t'il lors du prochain stream ? </h1>
-    <nav>
-        Bienvenue sur la meilleure page de sondages pour comparer et deviner ce qu'il va ce passer sur la plateforme Twitch
-    </nav>
-
-
-    <div>
-
+    <div id="pdg">
+        <p> Bienvenue sur la meilleure page de sondages pour comparer et deviner ce qu'il va ce passer sur la plateforme Twitch</p>
     </div>
 
 
 
+    <div id="Twitch">
+        <img id="LogoTwitch" src="img/Twitch.png">
+    </div>
 
+
+
+    <div>
+        <h3> Nous sommes partenaires de cette plateforme de streaming pour vous offrir la meilleure expérience </h3>
+    </div>
 
 
 
